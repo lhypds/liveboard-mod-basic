308dd5c3dd267c845cdb0b455c9fd34f55697c42
Heyang Liu <lhypds@gmail.com>
2026-06-16 13:30:34 +0900

    Info

--- HeatMap/config.ts
+++ HeatMap/config.ts
@@ -1,11 +1,22 @@
 export const config = {
   i: "HeatMap",
   title: { en: "Tokyo Heatmap", ja: "東京ヒートマップ", zh: "东京热力图" },
-  info: {
-    dataSource: { en: "STR + internal PMS", ja: "STR + 自社PMS", zh: "STR + 内部 PMS" },
-    refreshFrequency: { en: "Every hour", ja: "1時間ごと", zh: "每小时" },
-    refreshAgeMinutes: 24,
-  },
+  refreshAgeMinutes: 24,
+  info: [
+    {
+      title: { en: "Data", ja: "データ", zh: "数据" },
+      items: [
+        {
+          key: { en: "Data Source", ja: "データソース", zh: "数据来源" },
+          value: { en: "STR + internal PMS", ja: "STR + 自社PMS", zh: "STR + 内部 PMS" },
+        },
+        {
+          key: { en: "Refresh Frequency", ja: "更新頻度", zh: "刷新频率" },
+          value: { en: "Every hour", ja: "1時間ごと", zh: "每小时" },
+        },
+      ],
+    },
+  ],
   x: 0,
   y: 0,
   w: 24,
--- Website/config.ts
+++ Website/config.ts
@@ -1,11 +1,22 @@
 export const config = {
   i: "Website",
   title: { en: "Website", ja: "ウェブサイト", zh: "网页组件" },
-  info: {
-    dataSource: { en: "External URL", ja: "外部URL", zh: "外部URL" },
-    refreshFrequency: { en: "Always live", ja: "常時ライブ", zh: "实时" },
-    refreshAgeMinutes: 0,
-  },
+  refreshAgeMinutes: 0,
+  info: [
+    {
+      title: { en: "Data", ja: "データ", zh: "数据" },
+      items: [
+        {
+          key: { en: "Data Source", ja: "データソース", zh: "数据来源" },
+          value: { en: "External URL", ja: "外部URL", zh: "外部URL" },
+        },
+        {
+          key: { en: "Refresh Frequency", ja: "更新頻度", zh: "刷新频率" },
+          value: { en: "Always live", ja: "常時ライブ", zh: "实时" },
+        },
+      ],
+    },
+  ],
   x: 0,
   y: 0,
   w: 20,
