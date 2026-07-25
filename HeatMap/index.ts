3b76808a9f5a3921d6fe2ae8f2d3b060697a09e8
Heyang Liu <lhypds@gmail.com>
2026-07-25 09:40:17 +0900

    Remove HeatMap

--- HeatMap/index.ts
+++
@@ -1,2 +0,0 @@
-export { default } from "./HeatMap";
-export { config } from "./config";
--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import type { ComponentType } from "react";
 import moduleConfig from "./modules.config.json" with { type: "json" };
-import * as HeatMap from "./HeatMap";
 import * as Website from "./Website";
 import * as Note from "./Note";
 
@@ -29,7 +28,6 @@ export type ModuleEntry = {
 };
 
 const avaliableModules: Record<string, ModuleEntry> = {
-  HeatMap: { component: HeatMap.default, config: HeatMap.config },
   Website: { component: Website.default, config: Website.config },
   Note: { component: Note.default, config: Note.config },
 };
